23095a47ad70c821d45809bf720f087b9293a66c
Akshavya Aggarwal <97899039+Akshavya2509@users.noreply.github.com>
2023-08-16 23:47:58 +0530

    Update restaurants.sol

--- restaurants.sol
+++ restaurants.sol
@@ -1,3 +1,68 @@
+// pragma solidity ^0.8.0;
+
+// contract Restaurants {
+//     // Restaurant name, address, outlets, and rating
+//     struct Restaurant {
+//         string name;
+//         string location;
+//         Outlet[] outlets; // Array to store outlets for each restaurant
+//     }
+
+//     Restaurant[] public restaurants;
+
+//     function addRestaurant(string memory _name, string memory _location) public {
+//         restaurants.push(Restaurant(_name, _location, new Outlet[](0))); // Initialize the outlets array
+//     }
+
+//     // Outlet
+
+//     struct Outlet {
+//         string name;
+//         string location;
+//         uint rating;
+//         FoodItem[] foodItems; // Array to store food items for each outlet
+//     }
+
+//     function addOutlet(uint restaurantIndex, string memory _name, string memory _location, uint _rating) public {
+//         require(restaurantIndex < restaurants.length, "Invalid restaurant index");
+//         restaurants[restaurantIndex].outlets.push(Outlet(_name, _location, _rating, new FoodItem[](0))); // Initialize the food items array
+//     }
+
+//     // Get the number of outlets for a specific restaurant
+//     function getOutletCount(uint restaurantIndex) public view returns (uint) {
+//         require(restaurantIndex < restaurants.length, "Invalid restaurant index");
+//         return restaurants[restaurantIndex].outlets.length;
+//     }
+
+//     // Get the details of a specific outlet for a restaurant
+//     function getOutletDetails(uint restaurantIndex, uint outletIndex) public view returns (string memory, string memory, uint) {
+//         require(restaurantIndex < restaurants.length, "Invalid restaurant index");
+//         require(outletIndex < restaurants[restaurantIndex].outlets.length, "Invalid outlet index");
+//         Outlet memory outlet = restaurants[restaurantIndex].outlets[outletIndex];
+//         return (outlet.name, outlet.location, outlet.rating);
+//     }
+
+//     // Get the number of restaurants
+//     function getRestaurantCount() public view returns (uint) {
+//         return restaurants.length;
+//     }
+
+//     // Add a food item to a specific outlet of a specific restaurant
+//     function addFoodItem(uint _restaurantIndex, uint _outletIndex, string memory _name, uint _price) public {
+//         require(_restaurantIndex < restaurants.length, "Invalid restaurant index");
+//         require(_outletIndex < restaurants[_restaurantIndex].outlets.length, "Invalid outlet index");
+
+//         // Add the food item to the specified outlet of the restaurant
+//         restaurants[_restaurantIndex].outlets[_outletIndex].foodItems.push(FoodItem(_name, _price));
+//     }
+
+//     // Food Item
+//     struct FoodItem {
+//         string name;
+//         uint price;
+//     }
+// }
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -6,40 +71,46 @@ contract Restaurants {
     struct Restaurant {
         string name;
         string location;
-        Outlet[] outlets; // Array to store outlets for each restaurant
+        uint[] outletIndices; // Array of indices pointing to outlets
     }
 
     Restaurant[] public restaurants;
+    Outlet[] public outlets; // Array to store all outlets
 
     function addRestaurant(string memory _name, string memory _location) public {
-        restaurants.push(Restaurant(_name, _location, new Outlet[](0))); // Initialize the outlets array
+        uint[] memory emptyOutletIndices; // Initialize an empty array of outlet indices
+        restaurants.push(Restaurant(_name, _location, emptyOutletIndices));
     }
 
     // Outlet
-
     struct Outlet {
         string name;
         string location;
         uint rating;
-        FoodItem[] foodItems; // Array to store food items for each outlet
+        uint[] foodItemIndices; // Array of indices pointing to food items
     }
 
     function addOutlet(uint restaurantIndex, string memory _name, string memory _location, uint _rating) public {
         require(restaurantIndex < restaurants.length, "Invalid restaurant index");
-        restaurants[restaurantIndex].outlets.push(Outlet(_name, _location, _rating, new FoodItem[](0))); // Initialize the food items array
+        
+        uint[] memory emptyFoodItemIndices; // Initialize an empty array of food item indices
+        outlets.push(Outlet(_name, _location, _rating, emptyFoodItemIndices));
+        restaurants[restaurantIndex].outletIndices.push(outlets.length - 1); // Store the index of the new outlet
     }
 
-    // Get the number of outlets for a specific restaurant
-    function getOutletCount(uint restaurantIndex) public view returns (uint) {
+   function getOutletCount(uint restaurantIndex) public view returns (uint) {
         require(restaurantIndex < restaurants.length, "Invalid restaurant index");
-        return restaurants[restaurantIndex].outlets.length;
+        return restaurants[restaurantIndex].outletIndices.length;
     }
 
     // Get the details of a specific outlet for a restaurant
     function getOutletDetails(uint restaurantIndex, uint outletIndex) public view returns (string memory, string memory, uint) {
         require(restaurantIndex < restaurants.length, "Invalid restaurant index");
-        require(outletIndex < restaurants[restaurantIndex].outlets.length, "Invalid outlet index");
-        Outlet memory outlet = restaurants[restaurantIndex].outlets[outletIndex];
+        require(outletIndex < restaurants[restaurantIndex].outletIndices.length, "Invalid outlet index");
+
+        uint outletId = restaurants[restaurantIndex].outletIndices[outletIndex];
+        Outlet memory outlet = outlets[outletId];
+        
         return (outlet.name, outlet.location, outlet.rating);
     }
 
@@ -47,14 +118,17 @@ contract Restaurants {
     function getRestaurantCount() public view returns (uint) {
         return restaurants.length;
     }
+FoodItem[] public foodItems; // Array to store all food items
 
     // Add a food item to a specific outlet of a specific restaurant
     function addFoodItem(uint _restaurantIndex, uint _outletIndex, string memory _name, uint _price) public {
         require(_restaurantIndex < restaurants.length, "Invalid restaurant index");
-        require(_outletIndex < restaurants[_restaurantIndex].outlets.length, "Invalid outlet index");
+        require(_outletIndex < restaurants[_restaurantIndex].outletIndices.length, "Invalid outlet index");
 
-        // Add the food item to the specified outlet of the restaurant
-        restaurants[_restaurantIndex].outlets[_outletIndex].foodItems.push(FoodItem(_name, _price));
+        uint outletId = restaurants[_restaurantIndex].outletIndices[_outletIndex];
+        outlets[outletId].foodItemIndices.push(foodItems.length); // Store the index of the new food item
+        
+        foodItems.push(FoodItem(_name, _price)); // Add the food item to the foodItems array
     }
 
     // Food Item
