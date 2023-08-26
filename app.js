4ea9309d29e12829333c2efa36a0e99d40028f25
Akshavya Aggarwal <97899039+Akshavya2509@users.noreply.github.com>
2023-08-26 22:16:22 +0530

    Update app.js
    Rectified the errors that i have been facing

--- app.js
+++ app.js
@@ -1,85 +1,417 @@
 // Assuming you have the ABI and address of the smart contracts
-const restaurantsContractABI = [/* Your ABI here */];
-const restaurantsContractAddress = 'YOUR_RESTAURANTS_CONTRACT_ADDRESS';
+const restaurantsContractABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "foodItems",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "price",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "outlets",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "location",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "rating",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "restaurants",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "location",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_location",
+        "type": "string"
+      }
+    ],
+    "name": "addRestaurant",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "restaurantIndex",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_location",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_rating",
+        "type": "uint256"
+      }
+    ],
+    "name": "addOutlet",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "restaurantIndex",
+        "type": "uint256"
+      }
+    ],
+    "name": "getOutletCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "restaurantIndex",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "outletIndex",
+        "type": "uint256"
+      }
+    ],
+    "name": "getOutletDetails",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [],
+    "name": "getRestaurantCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_restaurantIndex",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_outletIndex",
+        "type": "uint256"
+      },
+      {
+        "internalType": "string",
+        "name": "_name",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_price",
+        "type": "uint256"
+      }
+    ],
+    "name": "addFoodItem",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "outletIndex",
+        "type": "uint256"
+      }
+    ],
+    "name": "getFoodItemCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "outletIndex",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "itemIndex",
+        "type": "uint256"
+      }
+    ],
+    "name": "getFoodItems",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  }
+]
+const restaurantsContractAddress = '/*Your account address here*/';
 
 // Connect to the Ethereum network using Web3.js
-const web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
+const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545');
 
 // Get the instance of the smart contract
 const restaurantsContract = new web3.eth.Contract(restaurantsContractABI, restaurantsContractAddress);
 
 // Function to fetch restaurant data from the smart contract
-async function fetchRestaurants() {
-    const restaurantCount = await restaurantsContract.methods.getRestaurantCount().call();
+// Function to fetch restaurant data from the smart contract
+// Create a link element
+var link = document.createElement('link');
 
+// Set the attributes of the link element
+link.rel = 'icon';
+link.type = 'image/ico';
+link.href = 'favicon.ico';
+
+// Append the link element to the head section of the document
+document.head.appendChild(link);
+
+async function fetchRestaurants() {
+  try {
+    const restaurantCount = await restaurantsContract.methods.getRestaurantCount().call();
+    console.log("restaurants "+restaurantCount);
     for (let i = 0; i < restaurantCount; i++) {
-        const restaurant = await restaurantsContract.methods.getRestaurant(i).call();
-        const restaurantCard = createRestaurantCard(restaurant);
+      const restaurant = await restaurantsContract.methods.restaurants(i).call();
+      if (restaurant.name && restaurant.location) {
+        const restaurantCard = createRestaurantCard(restaurant, i);
         document.getElementById('restaurantList').appendChild(restaurantCard);
+      }
     }
+  } catch (error) {
+    console.error('Error fetching restaurant count:', error);
+  }
 }
 
 // Function to create a restaurant card
-function createRestaurantCard(restaurant) {
-    const card = document.createElement('div');
-    card.className = 'restaurant-card';
-    card.innerHTML = `
-        <h2 class="restaurant-name">${restaurant.name}</h2>
-        <p class="restaurant-location">${restaurant.location}</p>
-    `;
-    card.onclick = () => {
-        showOutletPage(restaurant.restaurant_id);
-    };
-    return card;
+function createRestaurantCard(restaurant, restaurantId) {
+  const card = document.createElement('div');
+  card.className = 'restaurant-card';
+  card.innerHTML = `
+    <h2 class="restaurant-name">${restaurant.name}</h2>
+    <p class="restaurant-location">${restaurant.location}</p>
+  `;
+  card.addEventListener('click', () => {
+    showOutletPage(restaurantId);
+  });
+  return card;
 }
 
 // Function to show the outlet page of a specific restaurant
 async function showOutletPage(restaurantId) {
-    const outlets = await restaurantsContract.methods.getOutlets(restaurantId).call();
-    const outletList = document.createElement('div');
-    outletList.className = 'outlet-list';
+  try {
+    const outletCount = await restaurantsContract.methods.getOutletCount(restaurantId).call();
+    // console.log("outlets "+outletCount);
+    if (outletCount > 0) {
+      const outletList = document.createElement('div');
+      outletList.className = 'outlet-list';
 
-    for (let i = 0; i < outlets.length; i++) {
-        const outletCard = createOutletCard(outlets[i]);
+      for (let i = 0; i < outletCount; i++) {
+        const outletDetails = await restaurantsContract.methods.getOutletDetails(restaurantId, i).call();
+        console.log(outletDetails);
+        const outletCard = createOutletCard(restaurantId, outletDetails, i);
         outletList.appendChild(outletCard);
+      }
+
+      const container = createPageContainer();
+      container.appendChild(outletList);
+
+      const backButton = createBackButton(showHomePage);
+      container.appendChild(backButton);
+
+      document.body.innerHTML = '';
+      document.body.appendChild(container);
+
+      // Attach click event handlers to outlet cards
+      // const outletCards = document.getElementsByClassName('outlet-card');
+      // Array.from(outletCards).forEach((outletCard, index) => {
+      //   outletCard.addEventListener('click', async () => {
+      //     const outletDetails = await restaurantsContract.methods.getOutletDetails(restaurantId, index).call();
+      //     showFoodItemsPage(outletDetails);
+      //   });
+      // });
+    } else {
+      console.log('No outlets available for this restaurant.');
     }
-
-    const container = document.createElement('div');
-    container.className = 'container';
-    container.appendChild(outletList);
-
-    // Add a back button to navigate back to the home page
-    const backButton = document.createElement('button');
-    backButton.innerText = 'Back';
-    backButton.onclick = showHomePage;
-    container.insertBefore(backButton, outletList);
-
-    document.body.innerHTML = '';
-    document.body.appendChild(container);
-
-    // Navigate to the food-items.html page when an outlet is clicked
-    const outletCards = document.getElementsByClassName('outlet-card');
-    for (let i = 0; i < outletCards.length; i++) {
-        const outletCard = outletCards[i];
-        outletCard.onclick = () => {
-            showFoodItemsPage(outlets[i]);
-        };
-    }
+  } catch (error) {
+    console.error('Error fetching outlet count:', error);
+  }
 }
 
+// Function to create an outlet card
+function createOutletCard(resId, outlet, outletId) {
+  const card = document.createElement('div');
+  card.className = 'outlet-card';
+  card.innerHTML = `
+    <h2 class="outlet-name">${outlet[0]}</h2>
+    <p class="outlet-location">${outlet[1]}</p>
+    <p class="outlet-rating">Rating: ${outlet[2]} stars</p>
+  `;
+  card.addEventListener('click', () => {
+    showFoodItemsPage(resId, outlet, outletId);
+  });
+  return card;
+}
+
+// Function to create a container for pages
+function createPageContainer() {
+  const container = document.createElement('div');
+  container.className = 'container';
+  return container;
+}
+
+// Function to create a back button
+function createBackButton(clickHandler) {
+  const backButton = document.createElement('button');
+  backButton.innerText = 'Back';
+  backButton.addEventListener('click', clickHandler);
+  return backButton;
+}
+
+
+
 // Function to show the food items page of a specific outlet
-async function showFoodItemsPage(outlet) {
+async function showFoodItemsPage(resId, outlet, outletId) {
     const foodItemsList = document.createElement('div');
     foodItemsList.className = 'food-item-list';
-
-    for (let i = 0; i < outlet.foodItems.length; i++) {
-        const foodItem = outlet.foodItems[i];
-        const itemPrice = web3.utils.fromWei(foodItem.price, 'ether');
+    const itemCount = await restaurantsContract.methods.getFoodItemCount(outletId).call();
+    for (let i = 0; i < itemCount; i++) {
+        const foodItem = await restaurantsContract.methods.getFoodItems(outletId, i).call();
+        const item = foodItem[0];
+        const itemPriceETH = foodItem[1] * 0.0000073;
         const listItem = document.createElement('div');
         listItem.className = 'food-item';
         listItem.innerHTML = `
-            <span class="food-item-name">${foodItem.name}</span>
-            <span class="food-item-price">Price: ${itemPrice} ETH</span>
+            <h4 class="food-item-name" style="color: white">${i+1}. Food Item ${i+1}: ${item}</h4>
+            <h4 class="food-item-price" style="color:white">   Price: Rs. ${foodItem[1]} /${itemPriceETH} ETH</h4>
             <button class="add-button" onclick="addToCart('${foodItem.name}', ${foodItem.price})">Add to Cart</button>
         `;
         foodItemsList.appendChild(listItem);
@@ -93,7 +425,7 @@ async function showFoodItemsPage(outlet) {
     const backButton = document.createElement('button');
     backButton.innerText = 'Back';
     backButton.onclick = () => {
-        showOutletPage(outlet.restaurant_id);
+        showOutletPage(resId);
     };
     container.insertBefore(backButton, foodItemsList);
 
@@ -112,31 +444,6 @@ async function showFoodItemsPage(outlet) {
 // Function to show the home page
 function showHomePage() {
     location.reload(); // Reload the page to show the home page again
-}
-
-// Function to create an outlet card
-function createOutletCard(outlet) {
-    const card = document.createElement('div');
-    card.className = 'outlet-card';
-    card.innerHTML = `
-        <h2 class="outlet-name">${outlet.name}</h2>
-        <p class="outlet-location">${outlet.location}</p>
-    `;
-    const foodItemList = document.createElement('ul');
-    foodItemList.className = 'food-item-list';
-    for (let i = 0; i < outlet.foodItems.length; i++) {
-        const foodItem = outlet.foodItems[i];
-        const itemPrice = web3.utils.fromWei(foodItem.price, 'ether');
-        const listItem = document.createElement('li');
-        listItem.innerHTML = `
-            <span class="food-item-name">${foodItem.name}</span>
-            <span class="food-item-price">Price: ${itemPrice} ETH</span>
-            <button class="add-button" onclick="addToCart('${foodItem.name}', ${foodItem.price})">Add to Cart</button>
-        `;
-        foodItemList.appendChild(listItem);
-    }
-    card.appendChild(foodItemList);
-    return card;
 }
 
 // Cart to store selected items
